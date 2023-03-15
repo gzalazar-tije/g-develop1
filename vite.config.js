0e7fbd06d18e8b9b2232466873a8c344761724a2
Gera <gzalazar@e2etravelsolutions.com>
2023-03-15 12:23:16 -0300

    Js. modificado

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://gzalazar-tije.github.io/'
+  base: 'https://gzalazar-tije.github.io/',
   plugins: [react()],
 })
