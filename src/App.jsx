c746ba76e008e4b5b0218847feaa98b835e34a78
Gera <gzalazar@e2etravelsolutions.com>
2023-03-15 14:28:00 -0300

    tittle mas link

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
-        <a href="https://reactjs.org" target="_blank">
+        <a href="https://www.bocajuniors.com.ar/" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
@@ -25,7 +25,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Sitio de Testing GitHub - Actions
       </p>
     </div>
   )
